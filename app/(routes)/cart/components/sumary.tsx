774e9ef4a69f3279b599e479b4ec5fb0e08de2da
asdiop159752 <nxt159753@gmail.com>
2023-07-09 16:27:11 +0700

    update not error

--- app/(routes)/cart/components/sumary.tsx
+++ app/(routes)/cart/components/sumary.tsx
@@ -33,13 +33,13 @@ const Sumary = () => {
     const response = await axios.post(
       `${process.env.NEXT_PUBLIC_API_URL}/checkout`,
       {
-        productIds: items.map((item) => item.id),
+      productIds: items.map((item) => item.id),
       }
-    );
+      );
 
     window.location = response.data.url;
   };
-  return (
+  return ( 
     <div
       className="mt-16 rounded-lg bg-gray-50 px-4 py-6 sm:p-6 lg:col-span-5 lg:mt-0 lg:p-8"
     >
@@ -49,7 +49,7 @@ const Sumary = () => {
       <div className="mt-6 space-y-4">
         <div className="flex items-center justify-between border-t border-gray-200 pt-4">
           <div className="text-base font-medium text-gray-900">Order total</div>
-         <Currency value={totalPrice} />
+        <Currency value={totalPrice} />
         </div>
       </div>
       <Button onClick={onCheckout} disabled={items.length === 0} className="w-full mt-6">
