1d6f56de3462b7e3f0265d2b65f294d67a661425
asdiop159752 <nxt159753@gmail.com>
2023-07-09 09:02:34 +0700

    Product preview modal components(Store)

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from 'next'
 import { Urbanist } from 'next/font/google'
 import Footer from '@/components/fotter'
 import Navbar from '@/components/navbar'
+import ModalProvider from '@/providers/modal-provider'
 
 
 const font = Urbanist({ subsets: ['latin'] })
@@ -20,6 +21,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={font.className}>
+        <ModalProvider />
         <Navbar />
         {children}
         <Footer />
